e18d849451c2edacd49dcd6370559ac151aa5047
AStandal <standal.andreas@gmail.com>
2024-12-30 00:39:39 +0100

    Now serving html!

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+const html = await Bun.file("./src/index.html").text();
+
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
       console.log(`Received request for: ${req.url}`);
-      return new Response("Hello World!");
+      return new Response(html, {
+        headers: {
+          "Content-Type": "text/html",
+        },
+      });
     },
   });
 
