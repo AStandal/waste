247236eb66b64e897551172f51f5e9380f964f33
AStandal <standal.andreas@gmail.com>
2024-12-29 17:18:03 +0100

    Cleaned up index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,8 @@ const server = Bun.serve({
     port: 3000,
     fetch(req) {
       console.log(`Received request for: ${req.url}`);
-      printHello();
-      incrementRequestCounter();
       return new Response("Hello World!");
     },
   });
 
 console.log(`Listening on http://localhost:${server.port} ...`);
-
-const printHello = () => {
-    console.log("Hello World!");
-}
-
-let requestCounter = 0;
-
-const incrementRequestCounter = () => {
-    requestCounter++;
-    console.log(`Request counter: ${requestCounter}`);
-}
-
-
